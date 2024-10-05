4f8c8b236c8000ff4fce82cbe373da9ba6dea3d4
pgimenez96 <pablo.gimenez.zorrilla@gmail.com>
2024-10-05 08:49:42 -0400

    feat: componentes para grilla de productos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,18 @@
-import { Title } from '@/components'
+import { ProductGrid, Title } from '@/components'
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products
 
 export default function Shop() {
   return (
-    <Title
-      title="Tienda"
-      subtitle="Todos los productos"
-      className="mb-2"
-    />
+    <>
+      <Title
+        title="Tienda"
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+
+      <ProductGrid products={ products } />
+    </>
   );
 }
