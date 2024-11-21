9df38472dcd892a563fe1193f69901569d15d514
pgimenez96 <pablo.gimenez.zorrilla@gmail.com>
2024-11-21 10:04:01 -0300

    feat: address store

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -3,6 +3,8 @@
 import clsx from 'clsx'
 import { useForm } from 'react-hook-form'
 import type { Country } from '@/interfaces'
+import { useAddressStore } from '@/store'
+import { useEffect } from 'react'
 
 type FormInputs = {
   firstName: string
@@ -22,14 +24,25 @@ type Props = {
 
 export const AddressForm = ({ countries }: Props) => {
 
-  const { handleSubmit, register, formState: { isValid } } = useForm<FormInputs>({
+  const { handleSubmit, register, formState: { isValid }, reset } = useForm<FormInputs>({
     defaultValues: {
       // TODO: leer de la base de datos
     }
   })
 
+  const address = useAddressStore(state => state.address)
+  const setAddress = useAddressStore(state => state.setAddress)
+
+  useEffect(() => {
+    if (address.firstName) {
+      reset(address)
+    }
+  }, [])
+  
+
   const onSubmit = (data: FormInputs) => {
     console.log({ data })
+    setAddress(data)
   }
 
   return (
