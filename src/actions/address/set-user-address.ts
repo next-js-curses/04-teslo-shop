938608374beeb3603b64c7539420ec0465990fd7
pgimenez96 <pablo.gimenez.zorrilla@gmail.com>
2024-11-22 11:22:06 -0300

    feat: actions para guardar o actualizar dirección de entrega

---
+++ src/actions/address/set-user-address.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import prisma from '@/lib/prisma'
+import type { Address } from '@/interfaces'
+
+export const setUserAddress = async(address: Address, userId: string) => {
+  try {
+    const newAddress = await createOrReplaceAddress(address, userId)
+
+    return {
+      ok: true,
+      address: newAddress
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      ok: false,
+      message: 'No se pudo grabar la dirección'
+    }
+  }
+}
+
+const createOrReplaceAddress = async (address: Address, userId: string) => {
+  try {
+    const storeAddress = await prisma.userAddress.findUnique({
+      where: { userId }
+    })
+
+    const addressToSave = {
+      userId: userId,
+      address: address.address,
+      address2: address.address2,
+      countryId: address.country,
+      firstName: address.firstName,
+      lastName: address.lastName,
+      phone: address.phone,
+      postalCode: address.postalCode
+    }
+
+    if (!storeAddress) {
+      const addressNew = await prisma.userAddress.create({
+        data: addressToSave
+      })
+      return addressNew;
+    }
+
+    const addressUpdated = await prisma.userAddress.update({
+      data: addressToSave,
+      where: { id: storeAddress.id }
+    })
+
+    return addressUpdated
+
+  } catch (error) {
+    console.error(error)
+    throw new Error('No se pudo grabar la dirección')
+  }
+}
